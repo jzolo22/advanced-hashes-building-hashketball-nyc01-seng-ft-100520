b0c906c6dd1c72b50b2902ac0905b1883e0c8e19
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-17 18:01:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -96,14 +96,14 @@ def game_hash
           :shoe => 14, 
           :points => 24,
           :rebounds => 12,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
         }, 
         
         {
-          :player_name => ,
+          :player_name => "",
           :number => ,
           :shoe => , 
           :points => ,
